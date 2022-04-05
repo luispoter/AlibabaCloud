95029795077d71696fd0de02343c57697cdc5336
txw <tanxiongwang@znv.com>
2022-04-05 23:35:18 +0800

    Initial commit

---
+++ src/test/java/com/poter/alibabacloud/AlibabaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poter.alibabacloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
